fec05bf2ccb91d40dc2f91aeae56696797811258
Leozzun <syt09159@gmail.com>
2026-02-09 16:05:06 +0900

    시나리오 3번 - GoodsService 수정

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -2,6 +2,11 @@ package kosta.service;
 
 public class GoodsService {
 	public void insert() {
-		System.out.println("insert");
+		System.out.println("insert call..");
+	}
+	
+	public void select() {
+		System.out.println("select 추가");
+		System.out.println("시나리오 3번 test");
 	}
 }
